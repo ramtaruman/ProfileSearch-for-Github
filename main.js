0ba0e1a287be665c8da9a5c492fb0dda4c767238
ramtaruman <sengupta.prakhar@gmail.com>
2022-03-15 20:49:42 +0530

    linked with api endpoints

--- main.js
+++ main.js
@@ -19,15 +19,15 @@ async function getuser(username)
 function createusercard(user){
     const cardhtml=`<div class="card">
     <div>
-        <img src="https://img.wattpad.com/e056b4a8161b61a28317f1532b26e26d095c3555/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f776174747061642d6d656469612d736572766963652f53746f7279496d6167652f5377374d65316164394c6b4a58673d3d2d3934303536323235352e313632643539633863656333336639323237313536313731343031392e6a7067?s=fit&w=720&h=720" alt="" class="avatar">
+        <img src="${user.avatar_url}" alt="${user.name}" class="avatar">
     </div>
     <div class="userinfo">
-        <h2>JohnDoe</h2>
-        <p>Bruh moment and sis momentBruh moment and sis momentBruh moment and sis momentBruh moment and sis momentBruh moment and sis momentBruh moment and sis moment</p>
+        <h2>${user.name}</h2>
+        <p>${user.bio}</p>
         <ul>
-            <li>300 <strong>Followers</strong></li>
-            <li>100 <strong>Following</strong></li>
-            <li>30 <strong>Repos</strong></li>
+            <li>${user.followers} <strong>Followers </strong></li>
+            <li>${user.following} <strong>Following </strong></li>
+            <li>${user.public_repos}<strong>Repos </strong></li>
 
         </ul>
         <div class="repos">
